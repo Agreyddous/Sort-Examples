ebc86746e8c7927e214386ed27b878dbab3abb12
Nando <fernandovbmgomes@hotmail.com>
2021-07-03 15:43:28 -0300

    Adding count sort

---
+++ Algorithms/CountSort.cs
@@ -0,0 +1,40 @@
+namespace Sort.Algorithms
+{
+	public partial class Sorter
+	{
+		public void CountSort(int[] vector)
+		{
+			int largestValue = _countLargestValue(vector);
+
+			int[] countVector = new int[largestValue + 1];
+
+			for (int index = 0; index < vector.Length; index++)
+				countVector[vector[index]]++;
+
+			int vectorIndex = 0;
+			int countVectorIndex = 0;
+
+			while (countVectorIndex < countVector.Length)
+			{
+				if (countVector[countVectorIndex] > 0)
+				{
+					vector[vectorIndex++] = countVectorIndex;
+					countVector[countVectorIndex]--;
+				}
+
+				else
+					countVectorIndex++;
+			}
+		}
+
+		private int _countLargestValue(int[] vector)
+		{
+			int largestValue = -1;
+
+			for (int index = 0; index < vector.Length; index++)
+				largestValue = vector[index] > largestValue ? vector[index] : largestValue;
+
+			return largestValue;
+		}
+	}
+}
